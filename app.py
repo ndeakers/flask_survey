bdc28585ec7564607c22f92a3ec2c756889b0d6b
Matt Riese <rise.matt@gmail.com>
2021-04-22 16:36:19 -0700

    finished part 3 and 4

--- app.py
+++ app.py
@@ -29,4 +29,30 @@ def show_question(question_num):
     choices = question_instance.choices
     return render_template('question.html',
                            current_question=current_question,
-                           choices=choices)
+                           choices=choices,
+                           question_num=question_num)
+
+
+@app.route('/answer', methods=['POST'])
+def handle_answer():
+    '''Takes in answer from user and adds value to responses[].
+    Redirects user to next question unless they are at end of survey,
+    then redirects to thank you page'''
+    responses.append(request.form.get('answer'))
+    print("responses =", responses)
+    previous_question_num = int(request.form.get('question_num'))
+    if previous_question_num < len(survey.questions)-1:
+        next_question_num = previous_question_num + 1
+        return redirect(f'/questions/{next_question_num}')
+    else:
+        return redirect('/thank_you')
+
+# we are getting the number of the next question in order to create the
+# route to redirect to (questions/next_quesiton_num). we will take the previous question number and
+# increment by one. BUT if the question number is the lenght of the qustions
+# list, then redirect to /thankyou
+
+
+@app.route('/thank_you')
+def say_thanks():
+    return render_template('thank_you.html', survey_title=survey.title)
